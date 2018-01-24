9ff9828e412ede0e3b982799d4028d01f12f4d58
mohamed khatab <hamosuper8@gmail.com>
2018-01-24 14:48:57 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -5,6 +5,23 @@
 
 function makeGrid() {
 
-// Your code goes here!
+var gridRowss = $('#input_height').val();
+var gridColss = $('#input_width').val();
+var table = $('#pixel-canvas');
+table.children().remove();
 
+for (i = 0; i < gridRowss; i++) {
+    table.append('<tr></tr>');
 }
+
+for (j = 0; j < gridColss; j++) {
+   $('tr').each(function(){
+       $('this').append("<td></td>")
+});
+   }
+}
+
+$('#sizePicker').on("submit", function(event) {
+        event.preventDefault();
+        makeGrid();
+        });
